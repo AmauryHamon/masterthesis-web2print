f7f9ada6e685e03970df583c619b373bb1047c18
Amaury Hamon <contact@amauryhamon.com>
2023-09-18 17:57:34 +0200

    remove branding + adjust meta

--- theme/amauryhamon/body.php
+++ theme/amauryhamon/body.php
@@ -15,38 +15,38 @@
     <div class="runningfolio">
         <span class="folio"></span>
         <!-- Logo ÉSAD Pyrénées -->
-        <img src="<?= $theme_url ?>/css/logo.png" alt="ESAD Pyrénées">
+        <!-- <img src="/css/logo.png" alt="ESAD Pyrénées"> -->
         <!-- Votre diplôme -->
         <span class="diploma"><?= $diploma ?></span>
     </div>
 
     <!-- les méta-données -->
     <div class="meta">
-        <!-- l’année YYYY – YYYY  -->
-        <div class="meta-year"><?= $year ?></div>
         <!-- Votre nom -->
         <div class="meta-name">
             <?= $student['thesis']?> by <a href="<?=$student['link']?>" target="_blank"><?= $student['name']?></a><br>    
             Tutored by <a href="<?=$tuteur['link']?>" target="_blank"><?= $tuteur['name']?></a>    
         </div>
         <div class="meta-data">
-        <!-- Votre diplôme, option et mention -->
-        <p>
-            <!-- Votre école -->
-            <a target="_blank" href="<?= $ecole['link'] ?>"><?= $ecole['name'] ?></a><br>
-            <?= $diploma ?> <br>
-            <?= $mention ?> 
-        </p>
+            <!-- Votre diplôme, option et mention -->
+            <p>
+                <!-- Votre école -->
+                <a target="_blank" href="<?= $ecole['link'] ?>"><?= $ecole['name'] ?></a><br>
+                <?= $diploma ?> <br>
+                
+            </p>
         </div>
+        <!-- l’année YYYY – YYYY  -->
+        <div class="meta-year"><?= $year ?></div>
     </div>
 
     <!-- les liens rapides: lire, imprimmer, télécharger -->
     <nav id="quicklinks">
-        <a href="#nav">Lire en ligne</a>
+        <a href="#nav">Read online</a>
         <!-- supprimer ce lien une fois le PDF généré : -->
-        <a href="?print" title="Web to print">Imprimer</a>
+        <a href="?print" title="Web to print">Print</a>
         <!-- Modifier l’URL dans config.php -->
-        <a href="<?= $pdf ?>">Télécharger</a>
+        <a href="<?= $pdf ?>">Download</a>
     </nav>
 </header>
 
